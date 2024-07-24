2939e08f67d4d6fc78da94071f35219aba5930e3
Fabian Enrique Suarez Carvajal <inge.fabiansuarez@gmail.com>
2024-07-24 15:15:45 -0500

    se crearon las migraciones para los debts

---
+++ database/migrations/2024_07_24_201424_create_debts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('debts', function (Blueprint $table) {
+            $table->id();
+            $table->string('type'); // 'Me prestan' o 'Yo presto'
+            $table->decimal('amount', 10, 2);
+            $table->string('description')->nullable();
+            $table->string('Who');
+            $table->date('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('debts');
+    }
+};
